fcdbcece4e69f7603145295d0718fe674a6e6888
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-12-13 12:51:03 -0500

    Updating queries

--- src/sql/query_remote_function.sql
+++ src/sql/query_remote_function.sql
@@ -1,5 +1,5 @@
 SELECT
-  URI AS image_input,
-  `${project_id}.${dataset_id}`.${remote_function_name}(URI) AS image_description
+  uri AS image_input,
+  `${project_id}.${dataset_id}`.${remote_function_name}(uri) AS image_description
 FROM
   `${project_id}.${dataset_id}`.${object_table_id}
