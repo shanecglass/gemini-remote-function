dfd990787f6074ca869d32fc17302a14e90bfd1b
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-12-13 15:05:07 -0500

    troubleshooting

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,7 @@ output "bigquery_editor_url" {
   value       = "https://console.cloud.google.com/bigquery?project=${module.project-services.project_id}&ws=!1m5!1m4!6m3!1s${module.project-services.project_id}!2s${google_bigquery_dataset.demo_dataset.dataset_id}"
   description = "The URL to launch the BigQuery editor with the sample query procedure opened"
 }
+
+output "test" {
+  value = data.google_client_config.current.access_token
+}
