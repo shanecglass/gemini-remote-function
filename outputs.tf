efb785037acc7a2fc9868e08816ce45b9e8700a2
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-12-13 14:48:04 -0500

    Remove testing

--- outputs.tf
+++ outputs.tf
@@ -23,25 +23,3 @@ output "bigquery_editor_url" {
   value       = "https://console.cloud.google.com/bigquery?project=${module.project-services.project_id}&ws=!1m5!1m4!6m3!1s${module.project-services.project_id}!2s${google_bigquery_dataset.demo_dataset.dataset_id}"
   description = "The URL to launch the BigQuery editor with the sample query procedure opened"
 }
-
-output "create_remote_function_query" {
-  value = templatefile("${path.module}/src/sql/provision_remote_function.sql", {
-    project_id          = module.project-services.project_id,
-    dataset_id          = google_bigquery_dataset.demo_dataset.dataset_id
-    bq_function_name    = "gemini_bq_demo"
-    region              = var.region
-    bq_connection_id    = var.connection_id
-    remote_function_url = google_cloudfunctions2_function.remote_function.service_config[0].uri
-    }
-  )
-}
-
-output "run_remote_function_query" {
-  value = templatefile("${path.module}/src/sql/query_remote_function.sql", {
-    project_id           = module.project-services.project_id,
-    dataset_id           = google_bigquery_dataset.demo_dataset.dataset_id
-    remote_function_name = google_cloudfunctions2_function.remote_function.name
-    object_table_id      = google_bigquery_table.object_table.table_id
-  })
-  description = "The URL to launch the in-console tutorial for the EDW solution"
-}
