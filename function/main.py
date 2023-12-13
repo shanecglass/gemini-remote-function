71220b028288737a2a6f2bb4465f3502bf952682
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-12-13 16:35:24 -0500

    Newline cleaning in model response

--- function/main.py
+++ function/main.py
@@ -32,8 +32,11 @@ def analyze_image(image_file):
       stream=True
   )
   for response in responses:
-    print(response.text)
-    output = json.loads(response.text)
+    output = response.text
+    output = output.strip()
+    output = output.split("\n")
+    output = " ".join(output)
+    print(output)
     return output
 
 def run_it(request):
@@ -43,7 +46,8 @@ def run_it(request):
     vertexai.init(project=project_id, location=region)
     file_to_analyze = list_url(request)
     image_description = analyze_image(file_to_analyze)
-    return_json = ({"replies": image_description})
+    return_value = json.loads(image_description)
+    return_json = ({"replies": return_value})
     return return_json
   except Exception as e:
     return json.dumps({"errorMessage": str(e)}), 400
