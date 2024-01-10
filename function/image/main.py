7a41ab845c60097f80dbf874e6ac1bd7b4bece63
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-10 11:37:12 -0500

    Prep for linting

--- function/image/main.py
+++ function/image/main.py
@@ -18,53 +18,58 @@ import os
 import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Part
 
+
 @functions_framework.http
 def list_url(request):
-  print(request)
-  try:
-    request_json = request.get_json()
-    calls = request_json['calls']
-    for call in calls:
-      image_url = str(call[0])
-      print(image_url)
-    return image_url
-  except Exception as e:
-    return json.dumps({"errorMessage": str(e)}), 400
+    print(request)
+    try:
+        request_json = request.get_json()
+        calls = request_json['calls']
+        for call in calls:
+            image_url = str(call[0])
+            print(image_url)
+        return image_url
+    except Exception as e:
+        return json.dumps({"errorMessage": str(e)}), 400
+
 
 def analyze_image(image_file):
-  gemini_pro_vision_model = GenerativeModel("gemini-pro-vision")
-  print(gemini_pro_vision_model)
-  image = Part.from_uri(
-      image_file, mime_type="image/jpeg")
-  print(image)
-  context = 'Describe and summarize this image. Use no more than 5 sentences to do so'
-  prompt = [context, image]
-  print(prompt)
-  response = gemini_pro_vision_model.generate_content(prompt, stream=False)
-  print(response)
-  output = response.text
-  output = output.strip()
-  output = output.split("\n")
-  output = " ".join(output)
-  print(output)
-  return output
+    gemini_pro_vision_model = GenerativeModel("gemini-pro-vision")
+    print(gemini_pro_vision_model)
+    image = Part.from_uri(
+        image_file, mime_type="image/jpeg")
+    print(image)
+    context = '''Describe and summarize this image.
+      Use no more than 5 sentences to do so'''
+    prompt = [context, image]
+    print(prompt)
+    response = gemini_pro_vision_model.generate_content(prompt, stream=False)
+    print(response)
+    output = response.text
+    output = output.strip()
+    output = output.split("\n")
+    output = " ".join(output)
+    print(output)
+    return output
+
 
 def check_string(input_string):
-  if not input_string:
-    return "Unable to generate description"
-  return input_string
+    if not input_string:
+        return "Unable to generate description"
+    return input_string
+
 
 def run_it(request):
-  try:
-    project_id = os.environ.get("PROJECT_ID")
-    region = os.environ.get("REGION")
-    vertexai.init(project=project_id, location=region)
-    file_to_analyze = list_url(request)
-    image_description = analyze_image(file_to_analyze)
-    return_value = []
-    result = check_string(image_description)
-    return_value.append(result)
-    return_json = json.dumps({"replies": return_value})
-    return return_json
-  except Exception as e:
-    return json.dumps({"errorMessage": str(e)}), 400
+    try:
+        project_id = os.environ.get("PROJECT_ID")
+        region = os.environ.get("REGION")
+        vertexai.init(project=project_id, location=region)
+        file_to_analyze = list_url(request)
+        image_description = analyze_image(file_to_analyze)
+        return_value = []
+        result = check_string(image_description)
+        return_value.append(result)
+        return_json = json.dumps({"replies": return_value})
+        return return_json
+    except Exception as e:
+        return json.dumps({"errorMessage": str(e)}), 400
--- function/text/main.py
+++ function/text/main.py
@@ -16,52 +16,53 @@ import functions_framework
 import json
 import os
 import vertexai
-from vertexai.preview import generative_models
 from vertexai.preview.generative_models import GenerativeModel
+
 
 @functions_framework.http
 def list_text_input(request):
-  print(request)
-  try:
-    request_json = request.get_json()
-    calls = request_json['calls']
-    for call in calls:
-      text_prompt = str(call[0])
-      print(text_prompt)
-    return text_prompt
-  except Exception as e:
-    return json.dumps({"errorMessage": str(e)}), 400
+    print(request)
+    try:
+        request_json = request.get_json()
+        calls = request_json['calls']
+        for call in calls:
+            text_prompt = str(call[0])
+            print(text_prompt)
+        return text_prompt
+    except Exception as e:
+        return json.dumps({"errorMessage": str(e)}), 400
 
 
 def generate_text_from_prompt(text_string):
-  text_model = GenerativeModel("gemini-pro")  # this is the text-to-text model
-  responses = text_model.generate_content(text_string,
-                                          stream=False
-                                          )
-  output = responses.text
-  output = output.strip()
-  output = " ".join(l for l in output.splitlines() if l)
-  print(output)
-  return output
+    # this is the text-to-text model
+    text_model = GenerativeModel("gemini-pro")
+    responses = text_model.generate_content(text_string,
+                                            stream=False
+                                            )
+    output = responses.text
+    output = output.strip()
+    output = " ".join(l for l in output.splitlines() if l)
+    print(output)
+    return output
 
 
 def check_string(input_string):
-  if not input_string:
-    return "Unable to generate description"
-  return input_string
+    if not input_string:
+        return "Unable to generate description"
+    return input_string
 
 
 def run_it(request):
-  try:
-    project_id = os.environ.get("PROJECT_ID")
-    region = os.environ.get("REGION")
-    vertexai.init(project=project_id, location=region)
-    text_to_analyze = list_text_input(request)
-    text_output = generate_text_from_prompt(text_to_analyze)
-    return_value = []
-    result = check_string(text_output)
-    return_value.append(text_output)
-    return_json = json.dumps({"replies": return_value})
-    return return_json
-  except Exception as e:
-    return json.dumps({"errorMessage": str(e)}), 400
+    try:
+        project_id = os.environ.get("PROJECT_ID")
+        region = os.environ.get("REGION")
+        vertexai.init(project=project_id, location=region)
+        text_to_analyze = list_text_input(request)
+        text_output = generate_text_from_prompt(text_to_analyze)
+        return_value = []
+        result = check_string(text_output)
+        return_value.append(result)
+        return_json = json.dumps({"replies": return_value})
+        return return_json
+    except Exception as e:
+        return json.dumps({"errorMessage": str(e)}), 400
