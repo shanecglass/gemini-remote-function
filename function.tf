50606e45ad712ad095fa45bb92265fd4d6d1efcf
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-10 11:56:38 -0500

    Formatting update

--- function.tf
+++ function.tf
@@ -64,8 +64,8 @@ resource "google_cloudfunctions2_function" "image_remote_function" {
   }
 
   service_config {
-    max_instance_count               = 10
-  # min_instance_count can be set to 1 to improve performance and responsiveness
+    max_instance_count = 10
+    # min_instance_count can be set to 1 to improve performance and responsiveness
     min_instance_count               = 0
     available_memory                 = "512mb"
     timeout_seconds                  = 300
@@ -101,8 +101,8 @@ resource "google_cloudfunctions2_function" "text_remote_function" {
   }
 
   service_config {
-    max_instance_count               = 10
-  # min_instance_count can be set to 1 to improve performance and responsiveness
+    max_instance_count = 10
+    # min_instance_count can be set to 1 to improve performance and responsiveness
     min_instance_count               = 0
     available_memory                 = "512mb"
     timeout_seconds                  = 300
